596256e506a7be52a7ebe5e71720016e4ed91f8b
EunJung803 <aimee3322@gmail.com>
2024-05-24 15:46:09 +0900

    Guestbook add

---
+++ mysite2/src/main/java/com/poscodx/mysite/controller/GuestbookServlet.java
@@ -0,0 +1,38 @@
+package com.poscodx.mysite.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.poscodx.mysite.dao.UserDao;
+import com.poscodx.mysite.vo.UserVo;
+
+public class GuestbookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		String action = request.getParameter("a");
+		
+		if("".equals(action)) {
+			
+		}
+		else if("".equals(action)) {
+			
+		}
+		else if("".equals(action)) {
+			
+		}
+		else {
+			request.getRequestDispatcher("/WEB-INF/views/guestbook/list.jsp").forward(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
