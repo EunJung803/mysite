c4c8568657ac98d0978b171e30fd52b8211fe645
EunJung803 <aimee3322@gmail.com>
2024-05-27 13:06:02 +0900

    mysite2 Refactoring #1

---
+++ mysite2/src/main/java/com/poscodx/mysite/controller/ActionServlet.java
@@ -0,0 +1,44 @@
+package com.poscodx.mysite.controller;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public abstract class ActionServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	protected abstract Action getAction(String actionName);
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String actionName = Optional.ofNullable(request.getParameter("a")).orElse("");
+		
+		Action action = getAction(actionName);
+		if(action == null) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+		
+		action.execute(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		super.doGet(request, response);
+	}
+	
+	
+	public static interface Action {
+
+		void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+		
+		
+	}
+	
+}
