00322672798ea88e965a930aa2659f5abcfea63b
EunJung803 <aimee3322@gmail.com>
2024-05-31 09:06:31 +0900

    board sql update

--- database/board.sql
+++ database/board.sql
@@ -12,13 +12,17 @@ from board a, user b
 where a.user_no = b.no
 order by g_no desc, o_no asc;
 
-select a.no, b.name, a.title, a.contents, date_format(a.reg_date, '%Y/%m/%d %h:%i:%s')
+select a.no, b.name, a.title, a.contents, date_format(a.reg_date, '%Y/%m/%d %h:%i:%s') as reg_date, a.user_no, a.hit, a.g_no, a.o_no, a.depth
 from board a, user b 
 where a.user_no = b.no
-and a.no = 3;
+order by g_no desc, o_no asc
+limit 0, 5;
 
 -- delete
 delete from board where no = 5;
 
 -- update
 update board set title = '하이하이', contents = 'ㅎㅅㅎ 히히' where no = 1;
+
+-- count
+select count(no) from board;
