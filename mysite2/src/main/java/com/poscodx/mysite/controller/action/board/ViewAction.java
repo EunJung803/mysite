ee8233e7789f94af7265638b3ffb10e1d5106b5c
EunJung803 <aimee3322@gmail.com>
2024-06-02 02:26:41 +0900

    cookie hit

--- mysite2/src/main/java/com/poscodx/mysite/controller/action/board/ViewAction.java
+++ mysite2/src/main/java/com/poscodx/mysite/controller/action/board/ViewAction.java
@@ -3,6 +3,7 @@ package com.poscodx.mysite.controller.action.board;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,7 +17,39 @@ public class ViewAction implements Action {
 		
 		String no = request.getParameter("no");
 		
-		new BoardDao().updateBoardHit(no);
+		boolean visitFlag = false;		// 기존 쿠키로 방문했는지 확인하기 위한 flag
+		
+		// 쿠키 읽기
+		Cookie[] cookies = request.getCookies();
+		
+		if(cookies != null && cookies.length > 0) {
+			for(Cookie cookie : cookies) {
+				
+				if("hit".equals(cookie.getName())) {		// 쿠키 중에 hit 있는지 확인
+					
+					// 해당 페이지를 접속한 적이 있는지 확인
+					if(cookie.getValue().contains(no)) {
+						visitFlag = true;
+					} else {
+						cookie.setValue(cookie.getValue() + "/" + no);		// 쿠키에 해당 게시물 번호가 없으면 추가
+						response.addCookie(cookie);
+						new BoardDao().updateBoardHit(no);		// 조회수 += 1
+						visitFlag = true;
+					}
+				}
+				
+			}
+		}
+		
+		// 쿠키 쓰기
+		if(!visitFlag) {		// 기존 쿠키로 방문한 적 없으면 -> 쿠키 생성
+			new BoardDao().updateBoardHit(no);		// 조회수 += 1
+			
+			Cookie cookie = new Cookie("hit", no);
+			cookie.setPath(request.getContextPath());
+			cookie.setMaxAge(24 * 60 * 60);
+			response.addCookie(cookie);
+		}
 		
 		request.setAttribute("vo", new BoardDao().findByNo(no));
 		
