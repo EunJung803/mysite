f003e7733bfe81947962defec34f0e2ca44d6f44
EunJung803 <aimee3322@gmail.com>
2024-06-05 11:46:17 +0900

    MyBatis #2

--- mysite3/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
+++ mysite3/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
@@ -43,32 +44,7 @@ public class GuestbookRepository {
 	
 	
 	public int insert(GuestbookVo vo) {
-		
-		int result = 0;
-		
-		try (
-			Connection conn = getConnection();
-			PreparedStatement pstmt1 = conn.prepareStatement("insert into guestbook(name, password, contents, reg_date) values(?, password(?), ?, now())");
-			PreparedStatement pstmt2 = conn.prepareStatement("select last_insert_id() from dual");
-		) {
-			
-			pstmt1.setString(1, vo.getName());
-			pstmt1.setString(2, vo.getPassword());
-			pstmt1.setString(3, vo.getContents());
-			result = pstmt1.executeUpdate();
-			
-			ResultSet rs = pstmt2.executeQuery();
-			
-			vo.setNo(rs.next() ? rs.getLong(1) : null);
-			
-			rs.close();
-		     
-		} catch (SQLException e) {
-			System.out.println("error: " + e);
-		}
-	    
-		return result;
-		
+		return sqlSession.insert("guestbook.insert", vo);
 	}
 	
 	
@@ -77,20 +53,7 @@ public class GuestbookRepository {
 	}
 	
 	public int deleteByNoAndPassword(Long no, String password) {
-		int result = 0;
-		
-		try (
-			Connection conn = getConnection();	
-			PreparedStatement pstmt = conn.prepareStatement("delete from guestbook where no = ? and password = password(?)");
-		){
-			pstmt.setLong(1, no);
-			pstmt.setString(2, password);
-			result = pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println("Error:" + e);
-		}
-		
-		return result;		
+		return sqlSession.delete("guestbook.deleteByNoAndPassword", Map.of("no", no, "password", password));	
 	}
 
 }
