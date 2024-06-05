36b0edc98eb219cca3b61005fd712548c4df3b3d
EunJung803 <aimee3322@gmail.com>
2024-06-05 13:15:55 +0900

    guestbook repository delete connection

--- mysite3/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
+++ mysite3/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
@@ -23,26 +23,6 @@ public class GuestbookRepository {
 		this.sqlSession = sqlSession;
 	}
 	
-	private Connection getConnection() throws SQLException {
-
-	    Connection conn = null;
-
-        try {
-    	    //1. JDBC Driver 로딩 
-			Class.forName("org.mariadb.jdbc.Driver");
-
-	        //2. 연결하기 
-			String url = "jdbc:mariadb://192.168.64.7:3306/webdb?charset=utf8";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-			
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패: " + e);
-		} 
-        
-	    return conn;
-	}
-	
-	
 	public int insert(GuestbookVo vo) {
 		return sqlSession.insert("guestbook.insert", vo);
 	}
