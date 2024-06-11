7bb8f4bf0f11254ca2febf42b825f7b8dcdb0bf2
EunJung803 <aimee3322@gmail.com>
2024-06-11 13:08:22 +0900

    site sql

---
+++ database/site.sql
@@ -0,0 +1,11 @@
+-- insert
+desc site;
+
+insert into site values(
+	null, 
+    'Mysite',
+	'안녕하세요. 김은정의 mysite에 오신 것을 환영합니다.', 
+    '/assets/images/winter.jpeg', 
+	'이 사이트는 웹 프로그램밍 실습과제 예제 사이트입니다.
+	메뉴는 사이트 소개, 방명록, 게시판이 있구요. 자바수업 + 데이터베이스 수업 + 웹프로그래밍 수업 배운 거 있는거 없는 거 다 합쳐서 만들어 놓은 사이트 입니다.'
+)
